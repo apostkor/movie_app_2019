68304b0028480c0861b7da2f19211895f00795eb
어포스트 (apost) <45118109+apostkor@users.noreply.github.com>
2020-01-31 22:44:21 +0900

    Updates

---
+++ precache-manifest.4f81abf54a2e1beabdf27e9ac0282c22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23aab696d670f934c74721436d244e5",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "4075c5a3d7483b091ad7",
+    "url": "/movie_app_2019/static/css/main.20980125.chunk.css"
+  },
+  {
+    "revision": "fe7e2cab0773743a5557",
+    "url": "/movie_app_2019/static/js/2.2df6c82d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2019/static/js/2.2df6c82d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4075c5a3d7483b091ad7",
+    "url": "/movie_app_2019/static/js/main.c4a11498.chunk.js"
+  },
+  {
+    "revision": "bc42afcb986eef44e7a7",
+    "url": "/movie_app_2019/static/js/runtime-main.30c24a73.js"
+  }
+]);
